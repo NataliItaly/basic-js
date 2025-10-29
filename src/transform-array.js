fa689e9bb75082e292cb401e44b7e3fc6bab4dbe
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-10-29 13:59:25 +0100

    resolve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,37 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let res = [];
+  let isToDiscard = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (isToDiscard) {
+      isToDiscard = false;
+      continue;
+    }
+    if (typeof arr[i] === 'number' && !isToDiscard) {
+      res.push(arr[i]);
+    }
+    else {
+      if (arr[i] === '--double-prev' && arr[i - 1] && typeof arr[i - 1] === 'number' && arr[i - 2] !== '--discard-next') {
+        res.push(arr[i - 1]);
+      }
+
+      if (arr[i] === '--double-next' && arr[i + 1] && typeof arr[i + 1] === 'number') {
+        res.push(arr[i + 1]);
+      }
+      if (arr[i] === '--discard-prev' && arr[i - 1] && typeof arr[i - 1] === 'number' && arr[i - 2] !== '--discard-next') {
+        res.pop();
+      }
+      if (arr[i] === '--discard-next' && arr[i + 1] && typeof arr[i + 1] === 'number') {
+        isToDiscard = true;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = {
